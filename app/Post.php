3143aa5e8271abbca88a9f5c80ae68472d1fe85b
yamamotonnnn <asoren1025@gmail.com>
2022-08-29 16:43:41 +0900

    ブログ投稿の編集

---
+++ app/Post.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+
+class Post extends Model
+{
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+    public function getPaginateByLimit(int $Limit_count = 10)
+    {
+       return $this->orderBy('updated_at', 'DESC')->paginate($Limit_count);
+    }
+}
+

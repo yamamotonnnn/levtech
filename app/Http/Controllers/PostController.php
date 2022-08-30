ef95e4ad83f00f446d2c1aae0ac73a38d0614cf6
yamamotonnnn <asoren1025@gmail.com>
2022-08-30 14:46:34 +0900

    ブログ投稿削除処理

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use App\Http\Requests\PostRequest;
+
+class PostController extends Controller
+{
+    public function index(Post $post)
+    {
+        return view('index')->with(['posts' => $post->getPaginateByLimit()]);
+    }
+    
+    public function show(Post $post)
+    {
+        return view('show')->with(['post' => $post]);
+    }
+    
+    public function create()
+    {
+        return view('create');
+    }
+    
+    public function store(Post $post, PostRequest $request)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    
+    public function edit(Post $post)
+    {
+        return view('edit')->with(['post' => $post]);
+    }
+    
+    public function update(PostRequest $request, Post $post)
+    {
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
+
+        return redirect('/posts/' . $post->id);
+    }
+    
+    public function delete(Post $post)
+    {
+        $post->delete();
+        return redirect('/');
+    }
+
+}
+?>
